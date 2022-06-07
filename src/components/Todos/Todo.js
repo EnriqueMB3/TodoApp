2f03d9be505a6a520776c312c69553fc660a4d5e
Jesus Hervey <hervey159@hotmail.com>
2022-06-06 21:09:00 -0500

    Task-Design

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -2,16 +2,62 @@ import React from 'react'
 import { MdCheckCircleOutline,  MdClear } from "react-icons/md";
 import Chip from '@mui/material/Chip';
 import { Card, Grid } from '@mui/material';
+import { fontSize } from '@mui/system';
 
 export const Todo = () => {
+
+  const TaskContainer = {
+    marginTop: 20,
+    marginLeft: 20, 
+    overflow: 'auto',
+    height:'76px'
+}
+
+const exController = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingRight: 5,
+ 
+}
+
+const TitleTask = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems:'center',
+    marginLeft: 10,
+    fontSize: 15
+}
+
+const chipTitle = {
+  fontSize: 11,
+  marginTop: 8,
+
+  marginLeft: 20,
+  letterSpacing: 0.4,
+  height: '25px'
+}
+
+
   return (
-    <Grid item xs={2} sm={4} md={4} >  
-        <Card >
+    <Grid  item xs={3} sm={4} md={3.9} >  
+  
+        <Card style={TaskContainer}>
+        
+            <div style={exController}>
+            <MdClear/>
+            </div>
+            
+            <div style={TitleTask}>
             <MdCheckCircleOutline/>
             Correr 2km
-            <Chip label="Ejercicio" />
-            <MdClear/>
+            </div>
+
+        
+            <Chip style={chipTitle} label="Catalogo" />
+            
         </Card>
+      
     </Grid>
   )
 }
